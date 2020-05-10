3254fa9127daf563531c7726ca5bb291fac563e1
Olga <lili432@mail.ru>
2020-05-10 10:11:35 +0200

    Header shadow

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <div className="pt-2 pb-2 border-g-gradient">
+    <div className="shadow pt-2 pb-2 border-g-gradient">
       <div className="container">
         <h1 className="h2 text-turquoise">Train your Brain!</h1>
       </div>
