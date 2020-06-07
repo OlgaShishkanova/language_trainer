788e5adc3f88449332b3b407134bc09cd3e815fa
Olga <lili432@mail.ru>
2020-06-07 21:51:19 +0200

    auth draft

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './scss/custom.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./scss/custom.scss";
+//import AppProviders from "./context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <App />
+    {/* <AppProviders>
+      <App />
+    </AppProviders> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
