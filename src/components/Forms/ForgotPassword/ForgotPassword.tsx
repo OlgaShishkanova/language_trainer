7a1e14e148a8b41d5cc898fa5de156f23baee7a5
Olga <lili432@mail.ru>
2020-06-15 20:47:12 +0200

    query-string

--- src/components/Forms/ForgotPassword/ForgotPassword.tsx
+++ src/components/Forms/ForgotPassword/ForgotPassword.tsx
@@ -3,19 +3,17 @@ import { RouteComponentProps } from "@reach/router";
 import CreateNewPassword from "./CreateNewPassword";
 import RemindPasswordForm from "./RemindPasswordForm";
 import { useLocation } from "@reach/router";
-// import {parse} from "query-string"
+import {parse} from "query-string"
 
 const ForgotPassword: React.FC<RouteComponentProps> = () => {
-  //TODO: download query-string library
-
   const location = useLocation();
-  const search = location.search;
-  //const searchParams = parse(search);
-  console.log("search", search);
+  const searchParams = parse(location.search);
+  const token = searchParams?.token
 
+  //TODO: check token function
   //TODO: redirection to NotFound page if token doesn't work
 
-  return <div>{search ? <CreateNewPassword /> : <RemindPasswordForm />}</div>;
+  return <div>{token ? <CreateNewPassword /> : <RemindPasswordForm />}</div>;
 };
 
 export default ForgotPassword;
